26d7e9af58f541811e77b987f2be87ab424d6bc9
Stephen Adams <sa597@kent.ac.uk>
2017-05-05 22:26:43 +0100

    Now supporting case statements

--- src/PrettyExplodesPrint.hs
+++ src/PrettyExplodesPrint.hs
@@ -13,30 +13,26 @@ import Data.Maybe
 import System.IO.Unsafe
 import FastString
 import BasicTypes
+import Language.Haskell.GHC.ExactPrint as EP
 
 file :: FilePath
-file = "testing/s.hs"
+file = "testing/listfunctions.hs"
 
 
 run :: IO ()
 run = do
-  p <- runGhc (Just libdir) $ do
-    dflags <- getSessionDynFlags
-    let dflags' = foldl xopt_set dflags [Opt_Cpp, Opt_ImplicitPrelude, Opt_MagicHash]
-    setSessionDynFlags dflags'
-    target <- guessTarget file Nothing
-    setTargets [target]
-    load LoadAllTargets
-    modSum <- getModSummary $ mkModuleName "S"
-    parseModule modSum
-  let pSource = pm_parsed_source p
-      forest = makeTree pSource
---      s = drawVerticalForest forest
-  print $ length (hsmodDecls (unLoc pSource))
-  mapM_ (\t -> (putStrLn $ drawVerticalTree t) >> (putStrLn (replicate 112 '='))) forest
+  mPS <- EP.parseModule file
+  case mPS of
+    Left (_, s) -> putStrLn ("Error parsing file: " ++ s)
+    Right (anns, pSource) -> do
+      let forest = makeTree anns pSource
+      print $ length (hsmodDecls (unLoc pSource))
+      mapM_ (\t -> (putStrLn $ drawVerticalTree t) >> (putStrLn (replicate 112 '='))) forest
 
-makeTree :: ParsedSource -> Forest String
-makeTree (GHC.L _ mod) = everything (++) ([] `mkQ` (\e -> [comp e])) (hsmodDecls mod)
+
+
+makeTree :: Anns -> ParsedSource -> Forest String
+makeTree anns (GHC.L _ mod) = everything (++) ([] `mkQ` (\e -> [comp e])) (hsmodDecls mod)
   where
     comp :: HsBind RdrName -> Tree String 
     comp (GHC.FunBind (GHC.L _ id) _ matches _ _ _) =
@@ -57,18 +53,31 @@ makeTree (GHC.L _ mod) = everything (++) ([] `mkQ` (\e -> [comp e])) (hsmodDecls
       let ml = (exprC l)
           mr = (exprC r) in
         [Node "App" (ml ++ mr)]
-    exprC (L _ (OpApp lhs (L _ (HsVar nm)) _ rhs)) =
+    exprC (L _ (OpApp lhs op _ rhs)) =
       let ml = (exprC lhs)
-          mop = processRdr nm
+          mop = exactPrint op anns
           mr = (exprC rhs) in
         [Node ("Op: " ++ mop) (ml++mr)]
+    exprC (L _ (HsCase expr pats)) =
+      let cStr = [Node "Case Expression" (exprC expr)]
+          pStr = handleMG pats in
+        [Node ("Case") (cStr ++ pStr)]
+            
     exprC (L _ (HsPar e)) = exprC e
     exprC (L _ (SectionL l r)) = let ml = exprC l
                                      mr = exprC r in
                                    [Node "Section" (ml++mr)] 
     exprC (L _ (HsOverLit lit)) = [Node (processLit lit) []]
     exprC e = gmapQ ((Node "Error: exprC" []) `mkQ` (\ i -> Node "" (exprC i))) e
-    
+    handleMG :: MatchGroup RdrName (LHsExpr RdrName) -> Forest String
+    handleMG mg =
+      let alts = mg_alts mg in
+        map handleM alts
+      where handleM :: LMatch RdrName (LHsExpr RdrName) -> Tree String
+            handleM (L _ m) = let pStr = foldr (++) "" (map (\p -> exactPrint p anns) (m_pats m))
+                                  mGrhsE = something (Nothing `mkQ` (\e -> Just $ exprC e)) (m_grhss m) in
+              Node ("Match: " ++ pStr) $ fromMaybe [] mGrhsE                          
+                            
     debug :: MatchGroup RdrName (LHsExpr RdrName) -> Forest String
     debug e = unsafePerformIO ((putStrLn ("Failed on : " ++ showData Parser 3 e))
                              >> (return []))
